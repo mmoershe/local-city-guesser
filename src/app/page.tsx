565dcee298544935779b9cd2565d83eda8397c11
mmoreshe <mark_henri.moersheim@smail.th-koeln.de>
2024-09-07 15:11:14 +0200

    minor styling + mobile responsivesness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 export default async function Index() {
     return (
         <div className="w-screen h-screen bg-black grid place-items-center">
-            <div className="w-[50%] h-[50%] bg-transparent rounded flex flex-col border border-solid border-white">
+            <div className="w-[80%] h-[50%] bg-transparent rounded flex flex-col border border-solid border-white lg:w-[50%]">
                 <div className="w-full h-[50%] flex p-10 pb-5 justify-center items-center">
-                    <div className="group p-5 w-full h-full rounded border border-solid border-white grid place-items-center duration-1000 hover:bg-white">
+                    <div className="group cursor-pointer p-0 w-full h-full rounded border border-solid border-white grid place-items-center duration-1000 hover:bg-white">
                         <h1 className="uppercase text-white group-hover:text-black duration-1000" style={{ fontSize: 'clamp(1rem, 5vw, 5rem)' }}>spielen</h1>
                     </div>
                 </div>
                 <div className="w-full h-[50%] flex p-10 pt-5 justify-center items-center">
-                    <div className="group p-5 w-full h-full rounded border border-solid border-white grid place-items-center duration-1000 hover:bg-white">
+                    <div className="group cursor-pointer p-0 w-full h-full rounded border border-solid border-white grid place-items-center duration-1000 hover:bg-white">
                         <h1 className="uppercase text-white group-hover:text-black duration-1000" style={{ fontSize: 'clamp(1rem, 5vw, 5rem)' }}>hochladen</h1>
                     </div>
                 </div>
